6187ebe885fa61cea621a7aec67aa849b037b76d
Magdalena Stryszowska <madzi13@op.pl>
2023-06-14 21:22:35 +0200

    avtive remove

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-/* document.getElementById('test-button').addEventListener('click', function(){
+/*document.getElementById('test-button').addEventListener('click', function(){
   const links = document.querySelectorAll('.titles a');
   console.log('links:', links);
 }); /* guzik do linków */
 
 const titleClickHandler = function(event){
-  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log('event:', event);
+
+  const links = document.querySelectorAll('.titles a');
+
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
+
  /* remove class 'active' from all article links  */
  const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -15,31 +20,20 @@ const titleClickHandler = function(event){
  }
 
  /* add class 'active' to the clicked link */
- console.log('clickedElement:', clickedElement);
- clickedElement.classList.add('active');
 
  /* remove class 'active' from all articles */
- const activeArticles = document.querySelectorAll('.posts article.active');
-
- for(let activeArticle of activeArticles){
-   activeArticle.classList.remove('active');
- }
 
  /* get 'href' attribute from the clicked link */
- const articleSelector = clickedElement.getAttribute('href');
- console.log('articleSelector:', articleSelector);
+
 
  /* find the correct article using the selector (value of 'href' attribute) */
- const targetArticle = document.querySelector(articleSelector);
- console.log(targetArticle)
- console.log('targetArticle:', targetArticle);
+
 
  /* add class 'active' to the correct article */
- targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
- link.addEventListener('click', titleClickHandler);
+  link.addEventListener('click', titleClickHandler);
 }
